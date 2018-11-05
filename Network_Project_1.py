323b2f66e410f5c483dba040eaca983f71753b0c
shazii <32590686+shazii@users.noreply.github.com>
2018-11-05 16:03:48 +0800

    Included Login and Main Page

--- Network_Project_1.py
+++ Network_Project_1.py
@@ -12,5 +12,27 @@ app = Flask(__name__)
 def hello_world():
     return 'Welcome to Tele Tubbs API<br><br><a href="/login">PLEASE LOGIN<a>'
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] == 'username' and request.form['password'] == 'password':
+            return redirect(url_for('mainpage'))
+    return '''<form action="" method="post">
+        <input type="text" placeholder="Username" name="username">
+        <input type="password" placeholder="Password" name="password">
+        <input type="submit" value="Login">
+    </form>'''
+
+
+@app.route('/mainpage', methods=['GET'])
+def mainpage():
+    return 'Welcome to Tele Tubbs Main Page'
+
+
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
