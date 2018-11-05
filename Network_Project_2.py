1a9d679e3cd070986fa74e4196d5cf802884bddc
tracyyee <tracy.yee.enying@hotmail.com>
2018-11-05 20:41:37 +0800

    added form submission option and displays messages
    - can type messages in the input field and press the 'send' button to submit
    - if type 'clear' in input field and submit, list will be cleared
    - if not, will continue adding to list and printing entire list

--- Network_Project_2.py
+++ Network_Project_2.py
@@ -1,128 +1,40 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
+
+"""
+@app.route('/signup', methods = ['POST'])
+def signup():
+    comment = request.form['comment']
+    return render_template("index.html", data=comment)
 
 @app.route('/')
 def hello_world():
     author = "Tracy"
     name = "Fellow SUTD-ian"
     return render_template('index.html', author=author, name=name)
+"""
+comments = []
+
+@app.route('/', methods = ['GET', 'POST'])
+def hello_world():
+    if request.method == 'GET':
+        return render_template('index.html', data=comments)
+    if request.method == 'POST':
+        comment = request.form['comment']
+        if comment.upper() == 'CLEAR' and len(comment) != 0:
+            del comments[:]
+        else:
+            comments.append(comment)
+        return render_template("index.html", data=comments)
 
 if __name__ == "__main__":
-    app.run()
+    app.run()    
 
-    
 """
 from flask import Flask, redirect, url_for, request, json, jsonify
 app = Flask(__name__)
-
-contact_form = '''
-<html lang="en">
-<head>
-<title>Tele Tubbs</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-* {
-    box-sizing: border-box;
-}
-
-body {
-    font-family: Arial, Helvetica, sans-serif;
-}
-
-/* Style the header */
-header {
-    background-color: #666;
-    padding: 30px;
-    text-align: center;
-    font-size: 35px;
-    color: white;
-}
-
-/* Create two columns/boxes that floats next to each other */
-nav {
-    float: left;
-    width: 30%;
-    height: 300px; /* only for demonstration, should be removed */
-    background: #ccc;
-    padding: 20px;
-}
-
-/* Style the list inside the menu */
-nav ul {
-    list-style-type: none;
-    padding: 0;
-}
-
-article {
-    float: left;
-    padding: 20px;
-    width: 70%;
-    background-color: #f1f1f1;
-    height: 300px; /* only for demonstration, should be removed */
-}
-
-/* Clear floats after the columns */
-section:after {
-    content: "";
-    display: table;
-    clear: both;
-}
-
-/* Style the footer */
-footer {
-    background-color: #777;
-    padding: 10px;
-    text-align: center;
-    color: white;
-}
-
-/* Responsive layout - makes the two columns/boxes stack on top of each other instead of next to each other, on small screens */
-@media (max-width: 600px) {
-    nav, article {
-        width: 100%;
-        height: auto;
-    }
-}
-</style>
-</head>
-<body>
-
-<h2>CSS Layout Float</h2>
-<p>In this example, we have created a header, two columns/boxes and a footer. On smaller screens, the columns will stack on top of each other.</p>
-<p>Resize the browser window to see the responsive effect (you will learn more about this in our next chapter - HTML Responsive.)</p>
-
-<header>
-  <h2>Cities</h2>
-  <input type="submit" name="submit_button" value="Do Something">
-  <input type="submit" name="submit_button" value="Do Something Else">
-</header>
-
-<section>
-  <nav>
-    <ul>
-      <li><a href="#">London</a></li>
-      <li><a href="#">Paris</a></li>
-      <li><a href="#">Tokyo</a></li>
-    </ul>
-  </nav>
-  
-  <article>
-    <h1>London</h1>
-    <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
-    <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
-  </article>
-</section>
-
-<footer>
-  <p>Footer</p>
-</footer>
-
-</body>
-</html>
-'''
 
 @app.route('/')
 def hello_world():
@@ -144,10 +56,6 @@ def login():
 @app.route('/mainpage', methods=['GET','POST'])
 def mainpage():
   return contact_form
-
-
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
