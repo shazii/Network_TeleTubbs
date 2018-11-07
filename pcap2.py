07dc29439b212a29e20831dea45af5008e07cb8b
tracyyee <tracy.yee.enying@hotmail.com>
2018-11-07 20:35:00 +0800

    appends to pcap file instead of overwriting

--- pcap2.py
+++ pcap2.py
@@ -1,4 +1,12 @@
 from scapy.all import *
+from scapy.utils import PcapWriter
 
-pkts_list = sniff(filter="tcp port 5000 and IP host 127.0.0.1", timeout=20)
-wrpcap('trace/traffic.pcap', pkts_list)
+while True:
+  pkts_list = sniff(filter="tcp port 5000 and IP host 127.0.0.1", timeout=5)
+
+  # Overwrites pcap file
+  #wrpcap('trace/traffic.pcap', pkts_list)
+
+  # Appends to pcap file
+  pktdump = PcapWriter("trace/traffic.pcap", append=True, sync=True)
+  pktdump.write(pkts_list)
