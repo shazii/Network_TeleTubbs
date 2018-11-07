3a3cbd5707ca298c33a6c601a5691d747315d0ce
tracyyee <tracy.yee.enying@hotmail.com>
2018-11-07 20:50:17 +0800

    added empty pcap file

--- pcap2.py
+++ pcap2.py
@@ -2,7 +2,8 @@ from scapy.all import *
 from scapy.utils import PcapWriter
 
 while True:
-  pkts_list = sniff(filter="tcp port 5000 and IP host 127.0.0.1", timeout=5)
+  
+  pkts_list = sniff(filter="tcp port 5000 and IP host 127.0.0.1", timeout=2)
 
   # Overwrites pcap file
   #wrpcap('trace/traffic.pcap', pkts_list)
